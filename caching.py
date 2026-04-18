8835501d1ae7849aa2ce9b6920f18ee954fdd372
gohy907 <gohy2006@mail.ru>
2026-04-18 16:24:48 +0400

    feat: delete from cache

--- caching.py
+++ caching.py
@@ -1,3 +1,5 @@
+from pompilius import get_existing_profiles
+import os
 from gi.repository import Nautilus, GObject, Gio, GLib
 
 from urllib.parse import unquote, urlparse
@@ -11,7 +13,28 @@ class PompiliusCaching(GObject.GObject, Nautilus.MenuProvider):
         GObject.Object.__init__(self)
 
     def get_file_items(self, files):
-        if not files:
+
+        profiles = get_existing_profiles()
+
+        show_menu = False
+        profile = {
+            "title": "",
+            "mount_root": ""
+        }
+        for file in files:
+            uri = file.get_uri()
+            file_path = unquote(urlparse(uri).path)
+            print(profiles)
+            for title, p in profiles.items():
+                if file_path.startswith(p):
+                    profile["title"] = title
+                    profile["mount_root"] = p
+                    show_menu = True
+                    break
+                if show_menu:
+                    break
+
+        if not show_menu:
             return []
 
         # Первая опция: Закешировать
@@ -28,23 +51,28 @@ class PompiliusCaching(GObject.GObject, Nautilus.MenuProvider):
             label="Удалить из кеша",
             tip="Удалить локальную копию, оставив файл в облаке"
         )
-        item_remove.connect("activate", self.delete_from_cache, files)
+        item_remove.connect("activate", self.delete_from_cache, files, profile)
 
         # Просто возвращаем список — Nautilus сам добавит их в меню по порядку
         return [item_cache, item_remove]
 
-    def delete_from_cache(self, item, files):
-        mock_profile = "gohy"
-        for file in files:
-            mock_path = "./Мишки.jpg"
+    def delete_from_cache(self, item, files, profile):
+        profiles = get_existing_profiles()
 
+        mock_profile = profile["title"]
+
+        for file in files:
+            uri = file.get_uri()
+            absolute_path = unquote(urlparse(uri).path)
             try:
+                relative_path = os.path.relpath(
+                    absolute_path, profile["mount_root"]).replace(mock_profile, "")
                 bus.call_sync(
                     'org.zbus.pompiliusd',
                     '/org/zbus/pompiliusd',
                     'org.zbus.pompiliusd',
-                    'RemoveFromCache',
-                    GLib.Variant('(ss)', (mock_profile, mock_path)),
+                    'DeleteCachePath',
+                    GLib.Variant('(ss)', (mock_profile, relative_path)),
                     None,
                     Gio.DBusCallFlags.NONE,
                     -1,
